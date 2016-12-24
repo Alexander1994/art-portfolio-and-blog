c0f17b7dd81a2eddbcf79f45ae391b3f8fa9a4c9
Alex <little_al14@hotmail.com>
2016-12-23 20:45:58 -0400

    requirements added and django configured for postgres

--- annies_site/settings.py
+++ annies_site/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'annies_portfolio_site',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'annies_site.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'annie_db',
+        'USER': 'annie_db_admin',
+        'PASSWORD': 'portfolio09',
+        'HOST': 'localhost',
+        'POST': '',
     }
 }
 
