1815c702a4e99de263465c238b619f2e5ff8b3d0
Alex <little_al14@hotmail.com>
2017-01-17 19:54:49 -0400

    npm and custom cdn url loader added

---
+++ annies_portfolio_site/templatetags/cdn.py
@@ -0,0 +1,14 @@
+from django import template
+from django.conf import settings
+import json, os
+register = template.Library()
+
+@register.simple_tag
+def cdn_url(cdn):
+    cdn_url_map = {
+        "iscroll": "//cdnjs.cloudflare.com/ajax/libs/iScroll/{}/iscroll.min.js",
+    }
+    package_json_path = os.path.join(settings.BASE_DIR, "package.json")
+    with open(package_json_path) as package_json_file:
+        package_json = json.load(package_json_file)
+    return cdn_url_map[cdn].format(package_json['cdnDependencies'][cdn][1:])
